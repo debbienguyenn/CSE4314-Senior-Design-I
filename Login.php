2685d23dc87ebe09cb6a50397ed81d4251fe8594
tuyen788 <th180416@gmail.com>
2021-06-02 14:08:53 -0500

    login started

---
+++ Login.php
@@ -0,0 +1,25 @@
+<?php
+
+session_start();
+
+$conn= mysqli_connect('localhost:3306','root','', 'sdproject');
+mysqli_select_db($conn, 'registration');
+
+$email= filter_input(INPUT_POST, 'email');
+$psw= filter_input(INPUT_POST, 'psw');
+
+$s = "SELECT * from registration where email= '$email' && psw= '$psw'";
+
+$result = mysqli_query($conn, $s);
+$num = mysqli_num_rows($result);
+
+    if($num==1)
+    {
+        header('location:profile.html');
+    }
+    else
+    {
+        header('location:Login.html');
+    }
+mysqli_close($conn);
+?>
