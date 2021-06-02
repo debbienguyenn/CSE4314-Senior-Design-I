a2c3f294e44402087cfdb2fed63d0e24bac3f6bb
tuyen788 <th180416@gmail.com>
2021-06-02 10:56:43 -0500

    signUp

---
+++ action_page.php
@@ -0,0 +1,23 @@
+<?php
+$email= $_POST['email'];
+$psw= $_POST['psw'];
+$pswrepeat = $_POST['pswrepeat'];
+
+
+
+//Database Connection
+ 
+$conn= new mysqli('localhost','root','test');
+if($conn->connect_error){
+    die('Connection Failed : ' .$conn->connect_error);
+    }
+    else{
+        $stmt=$conn->prepare("insert into SignUp(email,psw,pswrepeat) values(?,?,?)");
+        $stmt->bind_param("sss",$email,$psw,$pswrepeat);
+        $stmt->execute();
+        echo "registration Successful";
+        $stmt->close();
+        $conn->close();
+
+    }
+?>
