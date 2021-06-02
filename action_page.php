6ddc122bd5ca6aafd6bbf46a6f4b85e431996828
tuyen788 <th180416@gmail.com>
2021-06-02 11:09:33 -0500

    sql

--- action_page.php
+++ action_page.php
@@ -12,7 +12,7 @@ if($conn->connect_error){
     die('Connection Failed : ' .$conn->connect_error);
     }
     else{
-        $stmt=$conn->prepare("insert into SignUp(email,psw,pswrepeat) values(?,?,?)");
+        $stmt=$conn->prepare("insert into SignUp(email,password,repeatPass) values(?,?,?)");
         $stmt->bind_param("sss",$email,$psw,$pswrepeat);
         $stmt->execute();
         echo "registration Successful";
