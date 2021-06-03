f6abbbd730ea65133c2c7519b827d1a631355c31
tuyen788 <th180416@gmail.com>
2021-06-03 13:06:53 -0500

    minor changes

--- Login.php
+++ Login.php
@@ -2,26 +2,28 @@
 
 session_start();
 
+//database connection
 $conn= mysqli_connect('localhost:3306','root','', 'sdproject');
 mysqli_select_db($conn, 'registration');
 
+//retrieve user inputs
 $email= filter_input(INPUT_POST, 'email');
 $psw= filter_input(INPUT_POST, 'psw');
 
+//validate credentials
 $s = "SELECT * from registration where email= '$email' && psw= '$psw'";
-
 $result = mysqli_query($conn, $s);
 $num = mysqli_num_rows($result);
 
-    if($num==1)
-    {  
-        echo '<script>alert("Login Sucessfully")</script>';
-        echo '<script>window.location=\'profile.html\'</script>';
-    }
-    else
-    {
-        echo '<script>alert("Incorrect Email or Password")</script>';
-        echo '<script>window.location=\'Login.html\'</script>';
+if($num==1)
+{  
+    echo '<script>alert("Login Sucessfully")</script>';
+    echo '<script>window.location=\'profile.html\'</script>';
+}
+else
+{
+    echo '<script>alert("Incorrect Email or Password")</script>';
+    echo '<script>window.location=\'Login.html\'</script>';
     }
 mysqli_close($conn);
 ?>
