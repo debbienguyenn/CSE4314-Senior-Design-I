34bd7898b2b2195bd24745eaa23eb25864607e89
tuyen788 <th180416@gmail.com>
2021-06-03 00:09:39 -0500

    minor changes

--- SignUp.php
+++ SignUp.php
@@ -2,9 +2,11 @@
 
 session_start();
 
+//database connection
 $conn= mysqli_connect('localhost:3306','root','', 'sdproject');
 mysqli_select_db($conn, 'registration');
 
+//retrieve user inputs
 $email= filter_input(INPUT_POST, 'email');
 $psw= filter_input(INPUT_POST, 'psw');
 $firstname = filter_input(INPUT_POST, 'firstname');
@@ -12,28 +14,28 @@ $lastname = filter_input(INPUT_POST, 'lastname');
 $username = filter_input(INPUT_POST, 'username');
 $birthday = filter_input(INPUT_POST, 'birthday');
 
+//check if account exists
 $s = "SELECT * from registration where email= '$email'";
-
 $result = mysqli_query($conn, $s);
 $num = mysqli_num_rows($result);
 
-    if($num==1)
+if($num==1)
+{
+    echo '<script>alert("Email or Username exists")</script>';
+    echo '<script>window.location=\'SignUp.html\'</script>';
+}
+else
+{
+    $reg = "INSERT INTO registration(email,psw,firstname, lastname, username, birthday) VALUES('$email','$psw','$firstname', '$lastname', '$username', '$birthday')";
+    if( mysqli_query($conn, $reg))
     {
-        echo '<script>alert("Email or Username exists")</script>';
-        echo '<script>window.location=\'SignUp.html\'</script>';
+        echo '<script>alert("Registration successully!")</script>';
+        echo '<script>window.location=\'Login.html\'</script>';
     }
     else
     {
-        $reg = "INSERT INTO registration(email,psw,firstname, lastname, username, birthday) VALUES('$email','$psw','$firstname', '$lastname', '$username', '$birthday')";
-       if( mysqli_query($conn, $reg))
-       {
-           echo '<script>alert("Registration successully!")</script>';
-            echo '<script>window.location=\'Login.html\'</script>';
-       }
-       else
-       {
-           echo"fail";
-       }
+        echo"fail";
     }
+}
 mysqli_close($conn);
 ?>
