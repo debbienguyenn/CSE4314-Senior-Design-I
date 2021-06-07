c4e2dd94345819142d7aebe4b092dfc9c3c2ce7b
tuyen788 <th180416@gmail.com>
2021-06-07 14:12:34 -0500

    put navbar and footer onto separate files, fixing the buttons on homepage

--- footer.php
+++ footer.php
@@ -1,20 +1,20 @@
 
 <footer>
     <div class="row justify-content-center">
-      <div class="col-1">
+      <div class="col-1" align="center">
         <h6>ChipNSalsa</h6>
       </div>
     </div>
 
     <div class="row justify-content-center">
-      <div class="col-4">
+      <div class="col-4" align="center">
         <font size="2" style="color: grey">Senior Design Project | University of Texas at Arlington | <a
             href="FAQs.php" style="color: grey"><u>FAQs</u></a> | Terms of Use </font>
       </div>
     </div>
 
     <div class="row justify-content-center">
-      <div class="col-3">
+      <div class="col-3" align="center">
         <font size="-1" style="color: grey">Designed and built by the <a href="team.php"
             style="color: grey"><u>ChipsNSalsa Team&copy;</u></a>.</font>
       </div>
--- homepage.php
+++ homepage.php
@@ -26,53 +26,32 @@
 <body>
   <section>
 
-    <div class="container">
-      <div class="navbar">
-        <a href="Homepage.php">
-          <image src="images/logo.jpg" class="logo"></image>
-        </a>
-        <nav style="width:1000px" class="navbar navbar-expand-lg navbar-light bg-light">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="Homepage.php">Home</a>
+    <?php
+      include('navbar.php');
+    ?>
 
-            <!--dropdown menu for video categories-->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-              aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                    data-bs-toggle="dropdown" aria-expanded="false">
-                    Videos
-                  </a>
-                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#">Youtube</a></li>
-                    <li><a class="dropdown-item" href="#">Anime</a></li>
-                    <li>
-                      <hr class="dropdown-divider">
-                    </li>
-                    <li><a class="dropdown-item" href="#">View All</a></li>
-                  </ul>
-                </li>
-                <li>
-                  <form class="d-flex">
-                    <input style="width:400px" class="form-control me-2" type="search" placeholder="Search"
-                      aria-label="Search">
-                    <button style="width:100px" class="btn btn-outline-success" type="submit">Search</button>
-                  </form>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div class="col-2">
-            <a href="Login.php">Login</a>
-            &emsp;
-            <a href="SignUp.php">Sign Up</a>
-          </div>
+          <!--Replace Login and Sign Up links when logged in-->
+          <?php
+            if(!isset($_SESSION['username']))
+            {
+              echo '<div class="col-2">
+              <a href="Login.php">Login</a>
+              &emsp;
+              <a href="SignUp.php">Sign Up</a>
+              </div>';
+            }
+            else
+            {
+              echo '<div class="col-2">
+              <a href="profile.php">Me</a>
+              &emsp;
+              <a href="Host.php">Host</a>
+              &emsp;
+              <a href="Logout.php">Log out</a>
+              </div>';
+            }
+          ?>
+          
 
         </nav>
       </div>
