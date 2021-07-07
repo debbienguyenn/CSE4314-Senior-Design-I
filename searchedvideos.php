9345833ea80486c5b8741856bb874b2227ee0033
lincoln38 <lincolnbhattarai38@gmail.com>
2021-07-07 13:27:35 -0500

    added search functionalities

---
+++ searchedvideos.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+     <title>Searched Videos</title>
+</head>
+<body background="images/search.jpg">
+    <div class="container">
+    <image src="images/logo.jpg" class="logo"></image>
+        <h1>Search video using a keyword</h1>
+        <form id="form">
+            <div class="form-group">
+                <input type="text" class="form-control" id="search" placeholder="search...">
+            </div>
+            <button class="form-group" class="btn btn-primary" value="Search"  >
+                click here to search videos
+            </button>
+        </form>
+        <div class="row">
+            <div class="col-md-12">
+                <div id="videos">
+
+                </div>
+            </div>
+        </div>
+    </div>
+      
+        
+        
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+                 <script src="script.js"></script>  
+</html>
